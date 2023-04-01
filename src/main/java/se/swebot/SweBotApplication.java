2daa406cde3db4b11b60325924dae9676e633240
Anestis Cheimonettos <white.house@sitsena.xyz>
2023-04-01 14:37:09 +0200

    Initial commit with spring

---
+++ src/main/java/se/swebot/SweBotApplication.java
@@ -0,0 +1,13 @@
+package se.swebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/swebot/SweBotApplicationTests.java
@@ -0,0 +1,13 @@
+package se.swebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
