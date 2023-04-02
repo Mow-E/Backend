6b47dc3f759f922243a9d79dda5c5d52c32396bd
Anestis Cheimonettos <white.house@sitsena.xyz>
2023-04-02 18:41:38 +0200

    + swagger

--- src/main/java/se/swebot/SweBotApplication.java
+++ src/main/java/se/swebot/SweBotApplication.java
@@ -1,8 +1,11 @@
 package se.swebot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "SweBot", version = "Sigma"))
 @SpringBootApplication
 public class SweBotApplication {
 
--- src/main/java/se/swebot/configs/SecurityConfig.java
+++ src/main/java/se/swebot/configs/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .antMatchers("/", "/login").anonymous()
                 .antMatchers("/home").hasAuthority("user")
                 .antMatchers("/h2-console", "/h2-console/**").permitAll()
+                .antMatchers("/swagger-ui", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                 .antMatchers("/auth/*").permitAll()
                 .anyRequest().authenticated()
             .and()
@@ -82,7 +83,7 @@ public class SecurityConfig {
                 .withDefaultSchema()
                 .withUser(User.withUsername("user")
                         .password(encoder.passwordEncoder().encode("pass"))
-                        .roles("USER"));
+                        .roles("ADMIN"));
     }
 
     @Bean
