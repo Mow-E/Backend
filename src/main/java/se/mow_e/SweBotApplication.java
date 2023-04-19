9db5a1ce2193e90c5f1e3ac74604063724657b91
Anestis Cheimonettos <white.house@sitsena.xyz>
2023-04-20 01:40:15 +0200

    + websockets, basic logic with topics

--- src/main/java/se/mow_e/SweBotApplication.java
+++ src/main/java/se/mow_e/SweBotApplication.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @OpenAPIDefinition(info = @Info(title = "SweBot", version = "Sigma"))
 @SpringBootApplication
+@EnableScheduling
 public class SweBotApplication {
 
     public static void main(String[] args) {
