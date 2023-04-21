a6338c64f750869295d945137b7bc813a6092c88
Steven ZHENG <steven@pylote.io>
2023-04-21 10:30:19 +0200

    [WIP] database crud

--- src/main/java/se/mow_e/controllers/AuthController.java
+++ src/main/java/se/mow_e/controllers/AuthController.java
@@ -33,6 +33,8 @@ public class AuthController {
         Map<String, Object> response = new HashMap<>();
 
         try {
+            System.out.println(loginRequest.getUsername());
+            System.out.println(loginRequest.getPassword());
             // Authenticate user
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
