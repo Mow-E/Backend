3dcfb5db89814dd2eaae2e889fc03b364b327d13
Steven ZHENG <steven@pylote.io>
2023-04-26 19:15:49 +0200

    [ADD] setup front end project with vite and mui template for dashboard

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import babel from '@rollup/plugin-babel'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // react(),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 })
