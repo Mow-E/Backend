a56f1514e6ad5aba313bf922ac25f78a17623cd0
SirSitsena <anestakos322anestis@gmail.com>
2023-05-04 22:46:07 +0200

    improved swagger

--- src/main/java/se/mow_e/controllers/MyControlous.java
+++ src/main/java/se/mow_e/controllers/MyControlous.java
@@ -1,9 +1,11 @@
 package se.mow_e.controllers;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SecurityRequirement(name = "auth")
 public class MyControlous {
 
     @GetMapping("/hello")
