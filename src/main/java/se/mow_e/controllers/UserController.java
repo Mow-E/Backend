93fcdcb5878495f33b3d0bbe185ce2f05e5df655
SirSitsena <anestakos322anestis@gmail.com>
2023-05-10 23:42:40 +0200

    + table for connecting the images to mower ids + endpoint for retrieving mowing sessions for a specific mower + security check so that only the owner of the mower can access its images and mowing history

--- src/main/java/se/mow_e/controllers/UserController.java
+++ src/main/java/se/mow_e/controllers/UserController.java
@@ -6,17 +6,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import se.mow_e.models.Coordinate;
+import se.mow_e.models.Mower;
+import se.mow_e.repository.CoordinateRepo;
+import se.mow_e.repository.MowerRepo;
 
-import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.Principal;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -24,20 +31,42 @@ import java.nio.file.Paths;
 public class UserController {
 
     @Autowired
-    ServletContext servletContext;
+    private MowerRepo mowerRepo;
+
+    @Autowired
+    private CoordinateRepo coordinateRepo;
+
+    @GetMapping(value = "/mower/history")
+    public List<Coordinate> getHistory(Principal principal) {   // Костыль ебаный - BAND-AID kill me for this
+
+        List<Mower> mowers = mowerRepo.findAllByUsername(principal.getName());
+
+        if (!mowers.isEmpty()) {
+            return coordinateRepo.findCoordinatesByMowerId(mowers.get(0).getMowerId());
+        } else {
+            return List.of();
+        }
+
+    }
 
     @GetMapping(value = "/mower/images/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<ByteArrayResource> get(@PathVariable String id) throws IOException {
+    public ResponseEntity<ByteArrayResource> getImage(@PathVariable String id, Principal principal) throws IOException {
 
-        Path path = Paths.get("data/images/" + id + ".jpg");
-        if (!Files.exists(path)) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
+        Mower mower = mowerRepo.findMowerByImagesContains(id);
+        if (mower != null && Objects.equals(mower.getUsername(), principal.getName())) {
 
-        ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(path));
+            Path path = Paths.get("data/images/" + id + ".jpg");
+            if (!Files.exists(path)) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found");
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .contentLength(inputStream.contentLength())
-                .body(inputStream);
+            ByteArrayResource inputStream = new ByteArrayResource(Files.readAllBytes(path));
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .contentLength(inputStream.contentLength())
+                    .body(inputStream);
+        } else {
+            throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT,"Image missing or not enough permissions");
+        }
     }
 
 }
